6c2ccebdc9cbea10a682311a880ed0a309c1a2c6
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2025-09-21 16:42:08 +0300

    coordinates -> location

--- src/WebApiSample/Warehouses/WarehouseResource.cs
+++ src/WebApiSample/Warehouses/WarehouseResource.cs
@@ -16,5 +16,5 @@ public sealed class WarehouseResource
   public string? Address { get; set; }
 
   [Required]
-  public LocationResource? Coordinates { get; set; }
+  public LocationResource? Location { get; set; }
 }
