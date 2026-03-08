5decae8c24812297766b315f41e48bc8bfd41884
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2026-03-08 21:01:21 +0300

    add no resource class

---
+++ src/WebApiSample/NoResource.cs
@@ -0,0 +1,6 @@
+﻿namespace WebApiSample;
+
+/// <summary>
+/// Use this class if you need to create a long-running operation that does not return a resource.
+/// </summary>
+public sealed class NoResource { }
